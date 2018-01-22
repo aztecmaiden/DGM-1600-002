e5d99a6574b68ac3889e09412c43bc25a9fb6eb8
Estephany <estephanycp6@gmail.com>
2018-01-22 09:53:15 -0700

    Created/Started Text Adventure

---
+++ TextAdventure/Assets/Adventure.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventure : MonoBehaviour {
+
+
+
+    public enum States { cell, window, gate};
+    public States myState;
+
+
+    Print(); ("There is an old crust Bed nearby.");
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
