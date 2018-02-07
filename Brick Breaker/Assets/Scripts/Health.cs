d0b580fd2523964b5a883bf4ac31be2d43c2d77e
Estephany <estephanycp6@gmail.com>
2018-02-07 09:54:07 -0700

    added health and physics

---
+++ Brick Breaker/Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public int health;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+
+        //if our health gets to zero 
+        if (health <= 0) ;
+        {
+            Destroy(gameObject);
+        }
+        //then destroy this object
+        
+    }
+}
