97bfcdc85e17fc05dcc19f2dc9f27f43a2032443
Estephany <estephanycp6@gmail.com>
2018-02-14 09:52:15 -0700

    added sound, added auto play

---
+++ Brick Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public AudioClip hitSound;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(bounce,new Vector3(0,0,0));
+
+
+    }
+}
--- Brick Breaker/Assets/Scripts/Paddle.cs
+++ Brick Breaker/Assets/Scripts/Paddle.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
+    public bool autoPlay;
+    private GameObject ball;
 
-	
-	// Update is called once per frame
-	void Update () {
+    private void Start()
+    {
+        ball = FindObjectOfType<Ball>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
         //get current paddle position
         Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
 
-        //get mouse horizontal positioned
-        float mousePositionInBlocks = Input.mousePosition.x / Screen.width*16 -8;
+        if (autoPlay)
+        {
+            paddlePosition.x = ball.transform.position.x;
+        }
+        else
+        {
+            //get mouse horizontal positioned
+            float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16 - 8;
 
-        paddlePosition.x = mousePositionInBlocks;
+            paddlePosition.x = mousePositionInBlocks;
+        }
 
         //set this paddle object to saved position
         gameObject.transform.position = paddlePosition;
