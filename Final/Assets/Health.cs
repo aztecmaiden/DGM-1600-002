15dcfb9b605cf7006c51e89e0404ad8e7c829b1b
Estephany <estephanycp6@gmail.com>
2018-03-12 09:57:41 -0600

    added a particle system for damage, also created a health script

---
+++ Final/Assets/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+    public ParticleSystem deathParticle;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+ 
+    
+   }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collided");
+        health--;
+        if (health <= 0)
+        {
+            //die
+            //play death particle
+            Instantiate(deathParticle);
+            Destroy(gameObject);
+        }
+    }
+}
