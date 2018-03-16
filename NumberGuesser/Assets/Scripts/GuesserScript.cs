dc0305ce59fd4c218582d59dae8d3972232e96cb
Estephany <estephanycp6@gmail.com>
2018-03-16 01:53:07 -0600

    Adding detail stuff to number guesser

--- NumberGuesser/Assets/Scripts/GuesserScript.cs
+++ NumberGuesser/Assets/Scripts/GuesserScript.cs
@@ -18,17 +18,18 @@ public class GuesserScript : MonoBehaviour {
 
 
         //Is the value GUESS
-        RidiculousThing();
+        GuessQuestion();
         //Instructions -Push these buttons
         print("Up arrow for higher, Down arrow for lower, Enter for correct.");
 
         max = max + 1;
 	}
 
-    private void RidiculousThing(){
+    private void GuessQuestion(){
         count --;
         //figure out how to get guess to be randomized**************************
-        guess = (max + min) / 2;
+        // guess = (max + min) / 2;
+        guess = Random.Range(min, max);
         print("Is the number " + guess + "?");
 
     }
@@ -38,19 +39,23 @@ public class GuesserScript : MonoBehaviour {
     //up arrow
        if (Input.GetKeyDown(KeyCode.UpArrow)){
             min = guess;
-            RidiculousThing();
+            GuessQuestion();
         }
         //down arrow
          else if (Input.GetKeyUp(KeyCode.DownArrow)) {
             max = guess;
-            RidiculousThing();
+            GuessQuestion();
 
         }
         //enter button
           else if (Input.GetKeyDown(KeyCode.Return)){
             print("The System wins!");
         }
+        // if count gets to zero you win***************
+        else if (count == 0)
+        {
+            print("The Indvidual Wins! \n");
         }
-    // if count gets to zero you win***************
     }
 }
+
