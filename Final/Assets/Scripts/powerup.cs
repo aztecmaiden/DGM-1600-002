7997e478c0c79c7dd84d22874b025cce3404a7eb
Estephany <estephanycp6@gmail.com>
2018-03-28 09:54:20 -0600

    worked on background image and health pack

---
+++ Final/Assets/Scripts/powerup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerup : MonoBehaviour {
+
+    public enum Power { Health, Speed};
+    public Power powerupType;
+    private SpriteRenderer rend;
+    public Sprite[] images;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>();
+        Debug.Log(rend);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (powerupType)
+        {
+            case Power.Health:
+                rend.sprite = images[0];
+                break;
+            case Power.Speed:
+                rend.sprite = images[1];
+                break;
+         }
+		
+	}
+}
