7975385aaf2dd406eae39c198e65ff02d826df76
Estephany <estephanycp6@gmail.com>
2018-04-11 09:58:22 -0600

    wow

--- Final/Assets/Scripts/LeveltoLoad.cs
+++ Final/Assets/Scripts/LeveltoLoad.cs
@@ -15,7 +15,7 @@ public class LeveltoLoad : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collider.GetComponent<PlayerMovement>())
+        if (GetComponent<PlayerMovement>())
         {
             //Game Over
             Debug.Log("GameOver");
--- Final/Assets/Scripts/Manager.cs
+++ Final/Assets/Scripts/Manager.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 
 public class Manager : MonoBehaviour {
 
+    private static Manager instance = null;
     public Text scoreboard;
     private int score;
     public Text levelDisplay;
     public Text livesDisplay;
     public int lives;
 
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this.gameObject.GetComponent<Manager>();
+        }
+        DontDestroyOnLoad(gameObject);
+    }
 
     public void IncrementScore(int value)
     {
@@ -22,8 +32,13 @@ public class Manager : MonoBehaviour {
 
     public void LoadLevel(string level)
     {
-
+        SceneManager.LoadScene(level);
     }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application.Quit");
+    }
 
 }
